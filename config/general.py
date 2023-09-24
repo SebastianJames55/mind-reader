95f5127029489cf53b2948d06f3c8aacb6224839
SebastianJames55 <sebastianjames.mec@gmail.com>
2023-09-24 14:41:53 +0530

    - connection to mindsdb cloud

--- config/general.py
+++ config/general.py
@@ -1,3 +1,10 @@
-# Configuration settings
+import os
+
+# Mindsdb account email
+MINDSDB_EMAIL = os.environ.get('MINDSDB_EMAIL')
+
+# Mindsdb account password
+MINDSDB_PASSWORD = os.environ.get('MINDSDB_PASSWORD')
+
 # Project name
 PROJECT_NAME = 'mind_reader_project'
---
+++ config/model.py
@@ -0,0 +1,9 @@
+import os
+# Model engine
+MODEL_ENGINE = 'openai'
+# Model name in engine
+MODEL_NAME = 'text-davinci-003'
+# OPENAI API KEY
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+# Model name in connector
+MODEL_NAME_IN_CONNECTOR = 'gpt_model'
